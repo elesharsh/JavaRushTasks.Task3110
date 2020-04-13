9facbcbc0e7173ed768da63c8229624fb92eb8ce
Andrey Shishitskiy <elesharsh@gmail.com>
2020-04-14 00:33:24 +0300

    Step 3 success

---
+++ Archiver.java
@@ -0,0 +1,15 @@
+package com.javarush.task.task31.task3110;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.nio.file.Paths;
+
+public class Archiver {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Введите полный путь архива:");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        ZipFileManager manager = new ZipFileManager(Paths.get(reader.readLine()));
+        System.out.println("Введите полный путь к файлу:");
+        manager.createZip(Paths.get(reader.readLine()));
+    }
+}

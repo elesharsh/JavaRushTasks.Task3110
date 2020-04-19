426551716be7f217a639831af94a5870778d6aad
Andrey Shishitskiy <elesharsh@gmail.com>
2020-04-19 17:20:24 +0300

    Step 10 success, add ZipFileManager.java

--- FileManager.java
+++ FileManager.java
@@ -4,8 +4,8 @@ import java.io.IOException;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.Collections;
-import java.util.LinkedList;
 import java.util.List;
 
 public class FileManager {
@@ -14,7 +14,7 @@ public class FileManager {
 
     public FileManager(Path rootPath) throws IOException {
         this.rootPath = rootPath;
-        fileList = new LinkedList<>();
+        this.fileList = new ArrayList<>();
         collectFileList(rootPath);
     }
 
@@ -24,11 +24,15 @@ public class FileManager {
 
     private void collectFileList(Path path) throws IOException {
         if (Files.isRegularFile(path)) {
+            // Добавляем только файлы
             fileList.add(rootPath.relativize(path));
         } else if (Files.isDirectory(path)) {
+            // Добавляем содержимое директории
+            // Рекурсивно проходимся по всему содержмому директории
+            // Чтобы не писать код по вызову close для DirectoryStream, обернем вызов newDirectoryStream в try-with-resources
             try (DirectoryStream<Path> directoryStream = Files.newDirectoryStream(path)) {
-                for (Path path1 : directoryStream) {
-                    collectFileList(path1);
+                for (Path file : directoryStream) {
+                    collectFileList(file);
                 }
             }
         }
